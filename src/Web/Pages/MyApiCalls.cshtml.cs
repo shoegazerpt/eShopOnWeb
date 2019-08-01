514828306575f82593dedb6dfd69b73f9136d0f6
Ricardo Fiel <rifiel@microsoft.com>
2019-08-01 07:42:13 +0100

    experiments

---
+++ src/Web/Pages/MyApiCalls.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Microsoft.eShopWeb.Web.Pages
+{
+    public class MyApiCallsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
